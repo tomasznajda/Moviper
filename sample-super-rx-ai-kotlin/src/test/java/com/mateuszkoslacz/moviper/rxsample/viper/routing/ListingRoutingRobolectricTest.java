b0d4f83ea50fabf8ed221d1ec666b4bc6fbf36b8
mkoslacz <mateusz.koslacz@grupawp.pl>
2017-01-05 12:59:58 +0100

    experimental implementation of routing activity starting unit test

--- sample-super-rx-ai-kotlin/src/test/java/com/mateuszkoslacz/moviper/rxsample/viper/routing/ListingRoutingRobolectricTest.java
+++ sample-super-rx-ai-kotlin/src/test/java/com/mateuszkoslacz/moviper/rxsample/viper/routing/ListingRoutingRobolectricTest.java
@@ -4,12 +4,17 @@ import android.content.Intent;
 
 import com.mateuszkoslacz.moviper.rxsample.BuildConfig;
 import com.mateuszkoslacz.moviper.rxsample.viper.entity.User;
+import com.mateuszkoslacz.moviper.rxsample.viper.presenter.ListingPresenter;
+import com.mateuszkoslacz.moviper.rxsample.viper.presenter.UserDetailsPresenter;
 import com.mateuszkoslacz.moviper.rxsample.viper.view.activity.ListingActivity;
 import com.mateuszkoslacz.moviper.rxsample.viper.view.activity.UserDetailsActivity;
+import com.mateuszkoslacz.moviper.server.ActivityStarter;
+import com.mateuszkoslacz.moviper.server.MoviperPresentersDispatcher;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
@@ -35,12 +40,34 @@ public class ListingRoutingRobolectricTest {
     }
 
     @Test
-    public void startUserDetailsActivity() throws Exception {
+    public void startUserDetailsActivityIntentTest() throws Exception {
         User user = new User();
         user.setLogin("testUser");
         mRouting.startUserDetailsActivity(user);
         Intent starter = new Intent(mListingActivity, UserDetailsActivity.class);
         starter.putExtra(UserDetailsActivity.Companion.getUSER_EXTRA(), user.getLogin());
         assertTrue(Shadows.shadowOf(mListingActivity).getNextStartedActivity().filterEquals(starter));
+    }
+
+    @Test
+    public void startUserDetailsActivityUnitTest() throws Exception {
+        MoviperPresentersDispatcher mPresentersDispatcher = Mockito.mock(MoviperPresentersDispatcher.class);
+        MoviperPresentersDispatcher.setInstance(mPresentersDispatcher);
+
+        User user = new User();
+        user.setLogin("testUser");
+
+        mRouting.startUserDetailsActivity(user);
+
+        Intent starter = new Intent(mListingActivity, UserDetailsActivity.class);
+        starter.putExtra(UserDetailsActivity.Companion.getUSER_EXTRA(), user.getLogin());
+
+        ActivityStarter activityStarter = ActivityStarter.newBuilder()
+                .withIntent(starter)
+                .withContext(mListingActivity)
+                .withPresenter(Mockito.mock(ListingPresenter.class)).build();
+
+
+        Mockito.verify(mPresentersDispatcher, Mockito.only()).startActivity(activityStarter);
     }
 }
